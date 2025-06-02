28b5110968ed6a995d86d8d34d4af84ae4233c34
Roberto Barrientos <barrientos.isc@gmail.com>
2025-06-02 11:40:53 -0600

    creación de model members e instalacion de libreria pillow

--- members/models.py
+++ members/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    last_name = models.CharField(max_length=100, verbose_name="Apellido")
+    date_of_birth = models.DateField(verbose_name="Fecha de Nacimiento")
+    address = models.CharField(max_length=255, verbose_name="Dirección")
+    city = models.CharField(max_length=100, verbose_name="Ciudad")
+    state = models.CharField(max_length=100, verbose_name="Estado")
+    zip_code = models.CharField(max_length=20, verbose_name="Código Postal")
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, unique=True, verbose_name="Número de Teléfono")
+    email = models.EmailField(unique=True, verbose_name="Correo Electrónico")
+    is_active = models.BooleanField(default=True, verbose_name="Activo")
+    joined_date = models.DateField(auto_now_add=True, verbose_name="Fecha de Ingreso")
+
+    def __str__(self):
+        return f"{self.name} {self.last_name} ({self.email})"
+
