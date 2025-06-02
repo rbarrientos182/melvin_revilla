ac68880742c923aaeb14966faac9d6f9f976a0ad
Roberto Barrientos <barrientos.isc@gmail.com>
2025-06-02 13:11:55 -0600

    se agrega members al admin

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    model = Member
+    list_display = ('id', 'name', 'email', 'joined_date')
+    search_fields = ('name', 'email')
+    list_filter = ('joined_date',)
+
+admin.site.register(Member, MemberAdmin)
+admin.site.site_header = "Members Admin"
+admin.site.site_title = "Members Admin Portal"
+admin.site.index_title = "Bienvenido al Portal Administrativo de Miembros"
+
